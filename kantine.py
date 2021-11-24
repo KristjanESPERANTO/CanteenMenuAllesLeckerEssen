486da9fd73d821ec52b83bec612a2ed880c0cf4f
Kristjan Esperanto <35647502+KristjanESPERANTO@users.noreply.github.com>
2021-11-25 00:56:37 +0100

    Update kantine.py

--- kantine.py
+++ kantine.py
@@ -1,16 +1,3 @@
-'''
-TODO:
-- Prüfen ob PDF-Files vom aktuellen und kommenden Monat vorhanden
-- Falls beide vorhanden: beide zusammenführen
-- Falls vom aktuellen Monat nicht vorhanden: Alte Daten nehmen und Hinweis erstellen
-- Allergene und Zutaten im Modul auflisten (JSON dafür manuell erstellen)
-- MagicMirror-Modul bauen
-- cron-Job erstellen, der die Daten regelmäßig holt
-- GitHub-Projekt erstellen
-- cron-Job erstellen, der aktuellen Daten vom GitHub-Projekt holt
-- Installationsanleitung für Python-Skript erstellen
-'''
-
 import pandas # To handle DataFrames
 import camelot.io as camelot # To get tables out of an PDF file
 
