63cbf1f4773dd0bc542646b23ea52f279c470085
Kristjan SCHMIDT <35647502+KristjanESPERANTO@users.noreply.github.com>
2021-11-29 21:55:23 +0100

    Update

--- kantine.py
+++ kantine.py
@@ -1,9 +1,10 @@
 import pandas # To handle DataFrames
 import camelot.io as camelot # To get tables out of an PDF file
+import json
 
 source_file_this_month = "https://www.alles-lecker-essen.de/wp-content/uploads/2021/10/Neutral1121.pdf"
 source_file_next_month = "https://www.alles-lecker-essen.de/wp-content/uploads/2021/11/neutral1221.pdf"
-source_file_test = "Neutral1121.pdf"
+source_file_test = "neutral1221.pdf"
 
 # Get all tabels in the PDF file - each page has one table
 tables_this_month = camelot.read_pdf(source_file_this_month, pages='all')
@@ -33,12 +34,32 @@ cleaned_table = cleaned_table.drop_duplicates()
 ## Remove rubbish rows
 cleaned_table = cleaned_table[cleaned_table.menu1 != "A"]
 cleaned_table = cleaned_table[cleaned_table.menu1 != "0"]
+## Clear cells with the value 0 in it
+cleaned_table = cleaned_table.replace(['0'], '')
 
 ## Set date as index
 cleaned_table = cleaned_table.set_index('date')
 
-## Output result
-print(cleaned_table)
+data_dict = {}
+
+for idx, row in cleaned_table.iterrows():
+    day_list = []
+    print(idx, row[0], row[1])
+    i = 0
+    for menu in row:
+      if menu != "":
+        i = i + 1
+        menu_object = {}
+        menu_object["Menü" + str(i)] = menu
+        day_list.append(menu_object)
+    data_dict[idx] = day_list
+    
+jsonString = json.dumps(data_dict, sort_keys=True, indent=2, ensure_ascii=False)
 
 # Write result to JSON file
-cleaned_table.to_json('speiseplan.json', force_ascii=False, orient='index', indent=2 )
+jsonFile = open("speiseplan.json", "w")
+jsonFile.write(jsonString)
+jsonFile.close()
+
+## Output result
+print(cleaned_table)
